a056d63bcfe972922693dffd795665397a052dc1
zhouwy19 <zhouwy1998@foxmail.com>
2020-07-16 10:10:03 +0800

    update cgan

--- jittorhub.py
+++ jittorhub.py
@@ -1,3 +1,4 @@
 pretrained_weights_url = {
-    "models.cyclegan.models:GeneratorResNet": "https://cg.cs.tsinghua.edu.cn/jittor/assets/build/generator_last.pkl",
+    "models.cgan.models:Generator": "https://cg.cs.tsinghua.edu.cn/jittor/assets/build/generator_last.pkl",
+    "models.cgan.models:Discriminator": "https://cg.cs.tsinghua.edu.cn/jittor/assets/build/discriminator_last.pkl",
 }
