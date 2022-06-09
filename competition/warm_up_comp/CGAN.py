6aa458f791f9dc946d8e7f1762086e227ec8f502
tpder <network@163.com>
2022-06-09 16:11:15 +0800

    info

--- competition/warm_up_comp/CGAN.py
+++ competition/warm_up_comp/CGAN.py
@@ -66,12 +66,15 @@ class Discriminator(nn.Module):
                                    nn.Linear(512, 512), 
                                    nn.Dropout(0.4), 
                                    nn.LeakyReLU(0.2), 
+                                   nn.Linear(512, 1)
                                    # TODO: 添加最后一个线性层，最终输出为一个实数
                                    )
 
     def execute(self, img, labels):
         d_in = jt.contrib.concat((img.view((img.shape[0], (- 1))), self.label_embedding(labels)), dim=1)
         # TODO: 将d_in输入到模型中并返回计算结果
+        validity = self.model(d_in)
+        return validity
 
 # 损失函数：平方误差
 # 调用方法：adversarial_loss(网络输出A, 分类标签B)
@@ -167,10 +170,12 @@ for epoch in range(opt.n_epochs):
         # ---------------------
 
         validity_real = discriminator(real_imgs, labels)
-        d_real_loss = adversarial_loss("""TODO: 计算真实类别的损失函数""")
+        #d_real_loss = adversarial_loss("""TODO: 计算真实类别的损失函数""")
+        d_real_loss = adversarial_loss(validity_real,valid)
 
         validity_fake = discriminator(gen_imgs.stop_grad(), gen_labels)
-        d_fake_loss = adversarial_loss("""TODO: 计算虚假类别的损失函数""")
+        #d_fake_loss = adversarial_loss("""TODO: 计算虚假类别的损失函数""")
+        d_fake_loss = adversarial_loss(validity_fake,fake)
 
         # 总的判别器损失
         d_loss = (d_real_loss + d_fake_loss) / 2
@@ -195,7 +200,7 @@ discriminator.eval()
 generator.load('generator_last.pkl')
 discriminator.load('discriminator_last.pkl')
 
-number = #TODO: 写入你注册时绑定的手机号（字符串类型）
+number = '18612116352'  #TODO: 写入你注册时绑定的手机号（字符串类型）
 n_row = len(number)
 z = jt.array(np.random.normal(0, 1, (n_row, opt.latent_dim))).float32().stop_grad()
 labels = jt.array(np.array([int(number[num]) for num in range(n_row)])).float32().stop_grad()
